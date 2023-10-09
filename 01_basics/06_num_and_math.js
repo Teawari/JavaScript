debe3a3259e0938b807c72ddf12d4c3805cd476d
Abhishek Kumar <104360224+Teawari@users.noreply.github.com>
2023-10-09 22:58:52 +0000

    maths and number

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,38 @@
+/*
+const score = 400;
+console.log(score);
+const score1 = new Number(400);
+console.log(score1);
+ const num = 1000;
+ console.log(num.toFixed(2));
+
+ const num2 = 123.954;
+ console.log(num2.toPrecision(5));
+
+ const num3 = 100000000;
+ console.log(num3.toLocaleString());   // convrts to english number system
+ console.log(num3.toLocaleString('en-in')); // convrts to indian number system
+ */
+
+
+
+
+ /* ++++++++++++++++++++++++ Maths ++++++++++++++++++++++++ */
+ /* console.log(Math);
+  console.log(Math.abs(-5));
+  console.log(Math.round(5.6));
+  console.log(Math.ceil(4.2));
+  console.log(Math.floor(4.9));
+  console.log(Math.sqrt(5));
+
+  console.log(Math.min(1,2,3,4,5))
+  console.log(Math.max(1,2,3,4,5))
+  */
+ console.log(Math.random())
+ console.log(Math.random()*10+1)
+ console.log(Math.floor(Math.random()*10+1))
+
+ const min = 10;
+ const max = 20;
+
+ console.log(Math.floor(Math.random()*(min-max+1))+ min);
