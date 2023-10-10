fe9a36fd2d0ece9349c6d7d9fbfcc8d5a613dae2
Abhishek Kumar <104360224+Teawari@users.noreply.github.com>
2023-10-10 16:57:05 +0000

    array basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,22 @@
+// Arrays 
+
+const xerox = [5,9,2,8,1]
+// console.log(xerox);
+// console.log(xerox[1]);
+
+// const xerox1 = new Array('ABHISHEK', 'TIWARI', 'TEA_WARI')
+// console.log(xerox1[2]);
+
+// Methods in Arrays
+ 
+// xerox.push(12)
+// xerox.push(19)
+// xerox.pop()
+
+const newXerox = xerox.join()
+console.log(xerox);
+
+console.log(newXerox);
+console.log(typeof newXerox);
+
+// Slice and Splice
