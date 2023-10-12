a37b08f59ebc7dea46d35e95f47b409602f6c87f
Abhishek Kumar <104360224+Teawari@users.noreply.github.com>
2023-10-12 18:02:27 +0000

    workflow

---
+++ 04_controlflow/01_one.js
@@ -0,0 +1,21 @@
+//  if (2 === '2'){
+//     console.log('executed')
+//  }
+
+//  const temperature = 50
+//  if (temperature <= 47){
+//    console.log('temperature is less than normal heat temperature')
+//  }
+//  else {
+//    console.log('temperature is more than normal heat temperature');
+//  }
+
+// const score = 100
+
+// if( score < 100){
+//    console.log(`you missed from hitting century`);
+// }
+// else{
+//    console.log(`You hit a century in the match against pakistan`)
+// }
+
