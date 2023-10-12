092c80f43291752bb8e902b2eb1f016665a3a831
Abhishek Kumar <104360224+Teawari@users.noreply.github.com>
2023-10-12 03:22:41 +0000

    arrows function

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -1,14 +1,43 @@
-const user = {
-    username: 'Abhishek',
-    price: 999,
+// const user = {
+//     username: 'Abhishek',
+//     price: 999,
 
-    welcomeMessage: function(){
-        console.log(`Hey Mr.${this.username}  , welcome to Akaymeals.com`);
-        console.log(this);
-    }
+//     welcomeMessage: function(){
+//         console.log(`Hey Mr.${this.username}  , welcome to Akaymeals.com`);
+//         console.log(this);
+//     }
     
-}
-user.welcomeMessage()
-user.username = 'Tiwari jee'
-user.welcomeMessage()
+// }
+// user.welcomeMessage()
+// user.username = 'Tiwari jee'
+// user.welcomeMessage()
 // console.log(this);
+
+// function chai(){
+//     let username = 'Abhishek'
+//     console.log(this.username);
+   
+// }
+// chai()
+
+// const chai = function(){
+//     let username = 'Abhishek'
+//     console.log(this.username);
+// }
+
+const chai = () => {
+    let username = 'Abhishek'
+    console.log(this.username);
+}
+// chai()
+
+
+// const difference = (num1,num2) =>{
+//     return num1 - num2
+// }
+
+
+const difference = (num1,num2) => num1 - num2 
+// we can use like this also const difference = (num1 - num2) => (num1 - num2)
+
+console.log(difference( 1000,547));
