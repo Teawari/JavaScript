94e457a588c637d417029d7f0fac98057401c3ae
Abhishek Kumar <104360224+Teawari@users.noreply.github.com>
2023-10-14 05:25:09 +0000

    high

---
+++ 05_Iterations/03_highrorderarray.js
@@ -0,0 +1,23 @@
+// for of loop
+
+// ["","",""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num);
+    
+}
+
+const greetings = "Hello Mr. Abhishek"
+
+for (const greet of greetings) {
+    console.log(greet);
+    
+}
+
+// Maps
+
+const map = new Map()
+map.
