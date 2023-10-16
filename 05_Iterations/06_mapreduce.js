a0c8ede247520087ebdf4f54a0991a8c34281b85
Abhishek Kumar <104360224+Teawari@users.noreply.github.com>
2023-10-16 03:35:08 +0000

    mapreduce

---
+++ 05_Iterations/06_mapreduce.js
@@ -0,0 +1,26 @@
+// const coding = ['js', 'ruby', 'java', 'javascript','cpp']
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// })
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//  const newNums = myNums.filter(  (num)=> num > 4)
+//  console.log(newNums);
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// //  const newNums = myNums.filter(  (num)=> {
+// //      return num > 4  // in scope we must use return value in function to get the return value
+
+// //  })
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if (num>4){
+//     newNums.push(num)
+//     }
+// })
+//  console.log(newNums);0
